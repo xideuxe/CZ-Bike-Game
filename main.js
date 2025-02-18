b7730b189662d989b98771572b33e8d9ed629e8b
xideuxe <xideuxe.salle@gmail.com>
2025-02-19 00:07:28 +0100

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,10 @@ class GameScene extends Phaser.Scene {
 
     preload() {
         this.load.image('ground', 'assets/ground.png');
-        this.load.image('czbike', 'assets/czbike.png');
+        this.load.spritesheet('czbike_anim', 'assets/czbike_spritesheet.png', {
+            frameWidth: 100,
+            frameHeight: 100
+        });
         this.load.image('bear', 'assets/bear.png');
         this.load.image('rug', 'assets/rug.png');
         this.load.image('coin', 'assets/coin.png');
@@ -15,8 +18,16 @@ class GameScene extends Phaser.Scene {
 
     create() {
         this.ground = this.add.tileSprite(400, 550, 800, 100, 'ground');
-        this.czBike = this.physics.add.sprite(100, 450, 'czbike').setScale(0.5);
+        this.czBike = this.physics.add.sprite(100, 450, 'czbike_anim').setScale(0.5);
         this.czBike.setCollideWorldBounds(true);
+
+        this.anims.create({
+            key: 'pedal',
+            frames: this.anims.generateFrameNumbers('czbike_anim', { start: 0, end: 3 }),
+            frameRate: 10,
+            repeat: -1
+        });
+        this.czBike.play('pedal');
 
         this.cursors = this.input.keyboard.createCursorKeys();
 
@@ -55,9 +66,10 @@ class GameScene extends Phaser.Scene {
 
     spawnObstacle() {
         const x = 800;
-        const y = 500;
-        const obstacleType = Math.random() > 0.5 ? 'bear' : 'rug';
-        let obstacle = this.obstacles.create(x, y, obstacleType);
+        const isBear = Math.random() > 0.5;
+        const y = isBear ? 500 : 350; // 500 pour les ours, 350 pour les tapis volants
+        
+        let obstacle = this.obstacles.create(x, y, isBear ? 'bear' : 'rug');
         obstacle.setVelocityX(-200);
     }
 
@@ -71,18 +83,22 @@ class GameScene extends Phaser.Scene {
     hitObstacle(czBike, obstacle) {
         this.physics.pause();
         czBike.setTint(0xff0000);
-        this.gameOver();
+
+        let message = obstacle.texture.key === 'bear' ? 'Bear Market' : 'Rug!';
+        this.add.text(400, 300, message, { fontSize: '64px', fill: '#FF0000' }).setOrigin(0.5);
+
+        this.time.addEvent({
+            delay: 2000,
+            callback: () => { this.scene.restart(); },
+            callbackScope: this,
+            loop: false
+        });
     }
 
     collectCoin(czBike, coin) {
         coin.disableBody(true, true);
         this.score += 10;
         this.scoreText.setText('Score: ' + this.score);
-    }
-
-    gameOver() {
-        this.add.text(400, 300, 'Game Over', { fontSize: '64px', fill: '#FF0000' }).setOrigin(0.5);
-        this.time.addEvent({ delay: 2000, callback: () => { this.scene.restart(); }, callbackScope: this, loop: false });
     }
 }
 
